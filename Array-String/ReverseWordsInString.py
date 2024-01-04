fa83613292d8983398cc2e114342740e85a30245
Kanika Rathi <132768785+kanika-07@users.noreply.github.com>
2024-01-04 18:19:05 +0530

    Create ReverseWordsInString.py

---
+++ Array-String/ReverseWordsInString.py
@@ -0,0 +1,26 @@
+# 151. Reverse Words in a String
+"""
+Given an input string s, reverse the order of the words.
+A word is defined as a sequence of non-space characters. The words in s will be separated by at least one space.
+Return a string of the words in reverse order concatenated by a single space.
+Note that s may contain leading or trailing spaces or multiple spaces between two words. 
+The returned string should only have a single space separating the words. Do not include any extra spaces.
+"""
+"""
+Time: O(n)
+Space: O(n)
+"""
+class Solution(object):
+    def reverseWords(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        return ' '.join(reversed(s.split()))
+"""
+Sample
+Input
+s = "the sky is blue"
+Output
+"blue is sky the"
+"""
