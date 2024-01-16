5cc67add86121ac99b677456357c8e3738d17063
Kanika Rathi <132768785+kanika-07@users.noreply.github.com>
2024-01-16 10:09:34 +0530

    Create MinStack.py

---
+++ Stack/MinStack.py
@@ -0,0 +1,60 @@
+# 155. Min Stack
+"""
+Design a stack that supports push, pop, top, and retrieving the minimum element in constant time.
+Implement the MinStack class:
+    MinStack() initializes the stack object.
+    void push(int val) pushes the element val onto the stack.
+    void pop() removes the element on the top of the stack.
+    int top() gets the top element of the stack.
+    int getMin() retrieves the minimum element in the stack.
+You must implement a solution with O(1) time complexity for each function.
+"""
+"""
+Time: O(1)
+Space: O(n)
+"""
+class MinStack(object):
+
+    def __init__(self):
+        self.stack = []
+
+    def push(self, val):
+        """
+        :type val: int
+        :rtype: None
+        """
+        mini = val if not self.stack else min(self.stack[-1][1], val)
+        self.stack.append([val, mini])
+
+    def pop(self):
+        """
+        :rtype: None
+        """
+        self.stack.pop()
+
+    def top(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][0]
+
+    def getMin(self):
+        """
+        :rtype: int
+        """
+        return self.stack[-1][1]
+
+# Your MinStack object will be instantiated and called as such:
+# obj = MinStack()
+# obj.push(val)
+# obj.pop()
+# param_3 = obj.top()
+# param_4 = obj.getMin()
+"""
+Sample
+Input
+["MinStack","push","push","push","getMin","pop","top","getMin"]
+[[],[-2],[0],[-3],[],[],[],[]]
+Output
+[null,null,null,null,-3,null,0,-2]
+"""
