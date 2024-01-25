aeb1d34961ce13fea50348853640ad03f97e766b
Kanika Rathi <132768785+kanika-07@users.noreply.github.com>
2024-01-25 09:54:59 +0530

    Create InsertInterval.py

---
+++ Intervals/InsertInterval.py
@@ -0,0 +1,35 @@
+# 57. Insert Interval
+"""
+You are given an array of non-overlapping intervals intervals where intervals[i] = [starti, endi] represent the start and the end of the ith interval and intervals is sorted in ascending order by starti. You are also given an interval newInterval = [start, end] that represents the start and end of another interval.
+Insert newInterval into intervals such that intervals is still sorted in ascending order by starti and intervals still does not have any overlapping intervals (merge overlapping intervals if necessary).
+Return intervals after the insertion.
+"""
+"""
+Time: O(n)
+Space: O(n)
+"""
+class Solution:
+    def insert(self, intervals: List[List[int]], newInterval: List[int]) -> List[List[int]]:
+        n = len(intervals)
+        ans = []
+        i = 0
+        while i<n and intervals[i][1] < newInterval[0]:
+            ans.append(intervals[i])
+            i += 1
+        while i < n and intervals[i][0] <= newInterval[1]:
+            newInterval[0] = min(newInterval[0], intervals[i][0])
+            newInterval[1] = max(newInterval[1], intervals[i][1])
+            i += 1
+        ans.append(newInterval)
+        while i<n:
+            ans.append(intervals[i])
+            i+=1
+        return ans
+"""
+Sample
+Input
+intervals = [[1,3],[6,9]]
+newInterval = [2,5]
+Output
+[[1,5],[6,9]]
+"""
